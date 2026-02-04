9a3c7d3cd8e45fa024f64ec5b6b8f926f45d3f4a
Waruna <warunaharshana78@gmail.com>
2026-02-04 22:47:35 +0530

    fix(android): use correct CapacitorVideoPlayerPlugin class for registration

--- android/app/src/main/java/com/lflix/app/MainActivity.java
+++ android/app/src/main/java/com/lflix/app/MainActivity.java
@@ -2,12 +2,12 @@ package com.lflix.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.jeep.plugin.capacitor.capacitorvideoplayer.CapacitorVideoPlayer;
+import com.jeep.plugin.capacitor.capacitorvideoplayer.CapacitorVideoPlayerPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(CapacitorVideoPlayer.class);
+        registerPlugin(CapacitorVideoPlayerPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
