dd6020112cbb1ace22280cc651527864f0b39896
Waruna <warunaharshana78@gmail.com>
2026-02-04 23:15:17 +0530

    fix(ui): remove sandbox attribute from iframe

--- app/components/LiveSports.tsx
+++ app/components/LiveSports.tsx
@@ -213,7 +213,6 @@ export default function LiveSports({ onClose }: Props) {
                         className="w-full h-full"
                         allowFullScreen
                         allow="autoplay; fullscreen; picture-in-picture"
-                        sandbox="allow-scripts allow-same-origin allow-presentation"
                         referrerPolicy="no-referrer"
                       />
                       {/* Live indicator */}
